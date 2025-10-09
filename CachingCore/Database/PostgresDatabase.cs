2ed2c0ff044b84b6120534135d6fbd36f7153a11
SamWaku <samuelwaku1st@gmail.com>
2025-10-09 11:38:04 +0200

    core: database

--- CachingCore/Database/PostgresDatabase.cs
+++ CachingCore/Database/PostgresDatabase.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using CachingCore.Modules.CacheTest;
+using Microsoft.EntityFrameworkCore;
+
+namespace CachingCore.Database;
+
+public class PostgresDatabase(DbContextOptions<PostgresDatabase> options) : DbContext(options)
+{
+    public DbSet<Cache> Caches { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.HasDefaultSchema("public");
+        base.OnModelCreating(builder);
+    }
+}
