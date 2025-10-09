f7531e8264d3038ea47c7778113a92d06f340812
SamWaku <samuelwaku1st@gmail.com>
2025-10-09 11:39:06 +0200

    dbextension

---
+++ CachingCore/Common/DatabaseExtension.cs
@@ -0,0 +1,22 @@
+﻿namespace CachingCore.Common;
+
+public static class DatabaseExtension
+{
+    public static void AddDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        var databaseConnection = configuration.GetConnectionString("Postgres");
+        
+        var dataSourceBuilder = new NpgsqlDataSourceBuilder(databaseConnection).EnableUnmappedTypes();
+        dataSourceBuilder.EnableDynamicJson();
+        dataSourceBuilder.RegisterEnumTypeConversion("public");
+        var dataSource = dataSourceBuilder.Build();
+        
+        services.AddDbContext<PostgresDatabase>(options =>
+        {
+            options.UseNpgsql(dataSource, opt => opt
+                    .EnableRetryOnFailure()
+                    .MigrationsHistoryTable("__EFMigrationsHistory", "public"))
+                .UseSnakeCaseNamingConvention();;
+        });
+    }
+}
