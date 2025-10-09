4ae8e136e04a1cc64a9eabf58a51b9299762ab45
SamWaku <samuelwaku1st@gmail.com>
2025-10-09 12:15:27 +0200

    core: entpoint to seed data

--- CachingCore/Modules/CacheTest/CacheSeedEnpoint.cs
+++ CachingCore/Modules/CacheTest/CacheSeedEnpoint.cs
@@ -1,6 +1,40 @@
-﻿namespace CachingCore.Modules.CacheTest;
+﻿using CachingCore.Database;
+using FastEndpoints;
 
-public class CacheSeedEnpoint
+namespace CachingCore.Modules.CacheTest;
+
+public class CacheSeedResponse
 {
-    
+    public string Message { get; set; }
+}
+
+public class CacheSeedEnpoint(PostgresDatabase database) : EndpointWithoutRequest<CacheSeedResponse>
+{
+    public override void Configure()
+    {
+        Get("seed-data");
+        Summary(s =>
+        {
+            s.Summary = "Seed data";
+        });
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var caches = new List<Cache>();
+        for (int i = 1; i <= 10000; i++)
+        {
+            caches.Add(new Cache
+            {
+                Description = $"Seeded cache entry #{i}",
+                CreatedOn = DateTimeOffset.UtcNow
+            });
+        }
+
+        await database.Caches.AddRangeAsync(caches);
+        await database.SaveChangesAsync();
+        
+        await Send.OkAsync(cancellation: ct);
+    }
 }
