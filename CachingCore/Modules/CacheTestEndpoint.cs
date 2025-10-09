4cc86f7b1eda725b1eb6d0d83637542bfb9f0f6b
SamWaku <samuelwaku1st@gmail.com>
2025-10-09 11:14:24 +0200

    core: test endpoint setup

--- CachingCore/Modules/CacheTestEndpoint.cs
+++ CachingCore/Modules/CacheTestEndpoint.cs
@@ -1,6 +1,26 @@
-﻿namespace CachingCore.Modules;
+﻿using FastEndpoints;
 
-public class CacheTestEndpoint
+namespace CachingCore.Modules;
+
+public class CacheResponse
 {
-    
+    public string Message { get; set; }
+}
+
+public class CacheTestEndpoint : Endpoint<EndpointWithoutRequest<CacheResponse>>
+{
+    public override void Configure()
+    {
+        Get("/CacheTest");
+        Summary(s =>
+        {
+            s.Summary = "Cache test";
+        });
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(EndpointWithoutRequest<CacheResponse> req, CancellationToken ct)
+    {
+        await Send.OkAsync("Cache test");
+    }
 }
--- CachingCore/Program.cs
+++ CachingCore/Program.cs
@@ -1,14 +1,24 @@
+using FastEndpoints;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddFastEndpoints();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("DefaultPolicy", policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.UseCors("DefaultPolicy");
+app.UseAuthentication();   
+app.UseAuthorization();
+app.UseFastEndpoints();
 
 app.UseHttpsRedirection();
 
