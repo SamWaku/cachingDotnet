c5491cb9d6a38fae9d21ef9077ee6872498bba3f
SamWaku <samuelwaku1st@gmail.com>
2025-10-09 11:22:32 +0200

    core: caching

--- CachingCore/Modules/CacheTest/Cache.cs
+++ CachingCore/Modules/CacheTest/Cache.cs
@@ -1,1 +1,8 @@
-﻿
+﻿namespace CachingCore.Modules.CacheTest;
+
+public class Cache
+{
+    public int Id { get; set; }
+    public required string Description { get; set; }
+    public DateTimeOffset CreatedOn { get; set; }
+}
