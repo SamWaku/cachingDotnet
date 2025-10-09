de7ff552ae05561e99d8df281304a172f5838e57
SamWaku <samuelwaku1st@gmail.com>
2025-10-09 13:08:05 +0200

    core: test cahes

--- CachingCore/Modules/CacheTest/GetSeedDataEndpoint.cs
+++ CachingCore/Modules/CacheTest/GetSeedDataEndpoint.cs
@@ -1,6 +1,48 @@
-﻿namespace CachingCore.Modules.CacheTest;
+﻿using CachingCore.Database;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
 
-public class GetSeedDataEndpoint
+namespace CachingCore.Modules.CacheTest;
+
+public class DefaultResponse<T> where T : class
 {
-    
+    public required int StatusCode { get; set; }
+    public required string Message { get; set; }
+    public List<T> Data { get; set; }
+    public List<string> Errors { get; set; } = [];
+}
+
+
+public class GetSeedDataEndpoint(PostgresDatabase database) : EndpointWithoutRequest<DefaultResponse<Cache>>
+{
+    public override void Configure()
+    {
+        Get("seeded-data");
+        Summary(s =>
+        {
+            s.Summary = "Get data, no caching";
+        });
+        AllowAnonymous();
+    }
+
+    public override async Task<DefaultResponse<Cache>> ExecuteAsync(CancellationToken ct)
+    {
+        var seeded_data = await database.Caches
+            .AsNoTracking()
+            .ToListAsync();
+        
+        
+        return new DefaultResponse<Cache>
+        { 
+            StatusCode = 200,
+            Message = "Success",
+            Data = seeded_data.Select(x => new Cache
+            {
+                Id = x.Id,
+                Description = x.Description,
+                CreatedOn = x.CreatedOn
+            }).ToList(),
+            Errors = null
+        };
+    }
 }
